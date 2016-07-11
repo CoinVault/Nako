a4606a00ff9f7928077cf79b6f48844b14dc64e3
Dan Gershony <dan.gershony@gmail.com>
2016-07-11 19:19:00 +0100

    Docker install sctips

---
+++ docker/docker-install.sh
@@ -0,0 +1,22 @@
+# prepare
+apt-get update
+apt-get -y install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" >> /etc/apt/sources.list.d/docker.list
+
+# linux-image-extra
+apt-get update
+apt-get -y install linux-image-extra-$(uname -r)
+
+# install
+apt-get -y install docker-engine
+service docker start
+
+# create group
+#groupadd docker
+usermod -aG docker $(whoami)
+
+#docker compose
+curl -L https://github.com/docker/compose/releases/download/1.8.0-rc2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
