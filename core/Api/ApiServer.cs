7654973ab78d4a449bf8acc071838229a82eacbf
Dan Gershony <dan@crypzo.com>
2016-09-09 10:52:06 +0100

    Injecting the IStorage to the api container

--- core/Api/ApiServer.cs
+++ core/Api/ApiServer.cs
@@ -21,8 +21,11 @@ namespace Nako.Api
     using Nako.Config;
     using System;
     using System.IO;
+    using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
+
+    using Nako.Storage;
 
     #endregion
 
@@ -46,6 +49,23 @@ namespace Nako.Api
             this.application = nakoApplication;
         }
 
+        ////public static IWebHost BuildApiServer(ContainerBuilder builder, NakoConfiguration config)
+        ////{
+        ////    var url = string.Format("http://+:{0}", config.SyncApiPort);
+
+        ////    var hostbuilder = new WebHostBuilder()
+        ////      .UseKestrel()
+        ////      .UseContentRoot(Directory.GetCurrentDirectory())
+        ////      .UseUrls(url)
+        ////      .ConfigureServices(serv =>
+        ////      {
+        ////          serv.Add(new ServiceDescriptor(typeof(ContainerBuilder), builder));
+        ////      })
+        ////      .UseStartup<Startup>();
+
+        ////    return hostbuilder.Build();
+        ////}
+
         public Task StartApi(IContainer container)
         {
             this.application.CreateApiToken();
@@ -63,7 +83,11 @@ namespace Nako.Api
                           .UseUrls(url)
                           .ConfigureServices(serv =>
                           {
+                              // this is a really ugly hack
+                              // todo: look in to injecting the container builder in to the api
                               serv.Add(new ServiceDescriptor(typeof(NakoConfiguration), container.Resolve<NakoConfiguration>()));
+                              serv.Add(new ServiceDescriptor(typeof(IStorage), container.Resolve<IStorage>()));
+
                           })
                           .UseStartup<Startup>();
 
@@ -95,7 +119,7 @@ namespace Nako.Api
             {
                 var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath);
-                Configuration = builder.Build();
+                this.Configuration = builder.Build();
             }
 
             public IConfigurationRoot Configuration { get; }
@@ -109,7 +133,7 @@ namespace Nako.Api
                 {
                     options.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
                 });
-                   
+
                 // Create the container builder.
                 // asp.net core autofac builds its container at this stage, 
                 // because the Nako server depends on the container being built already, 
