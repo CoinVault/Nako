edc337685d26ba4ac580648c5ca55bdfa022801a
Dan Gershony <dan.gershony@gmail.com>
2019-07-31 17:09:37 +0100

    Improving trx display

--- core/Nako.Ui.Blazor/GlobalData.cs
+++ core/Nako.Ui.Blazor/GlobalData.cs
@@ -9,7 +9,7 @@ namespace Nako.Ui.Blazor
     {
         public GlobalData()
         {
-            this.BlocksCache = new Dictionary<long, DataTypes.QueryBlock>();
+            this.BlocksCache = new LimitedDictionary<long, DataTypes.QueryBlock>();
         }
 
         public string Search { get; set; }
@@ -17,6 +17,40 @@ namespace Nako.Ui.Blazor
         /// <summary>
         /// TODO: fix this to have a max count in memory
         /// </summary>
-        public Dictionary<long, DataTypes.QueryBlock> BlocksCache;
+        public LimitedDictionary<long, DataTypes.QueryBlock> BlocksCache;
+    }
+
+    public class LimitedDictionary<TKey, TValue>
+    {
+        private Dictionary<TKey, TValue> dictionaty;
+        private Queue<TKey> queue;
+        private int MaxItems;
+
+        public LimitedDictionary()
+        {
+            this.dictionaty = new Dictionary<TKey, TValue>();
+            this.queue = new Queue<TKey>();
+            this.MaxItems = 50;
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if(!this.dictionaty.ContainsKey(key))
+            {
+                this.dictionaty.Add(key, value);
+                this.queue.Enqueue(key);
+
+                if (this.dictionaty.Count > this.MaxItems)
+                {
+                    var item = this.queue.Dequeue();
+                    this.dictionaty.Remove(item);
+                }
+            }
+        }
+
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return this.dictionaty.TryGetValue(key, out value);
+        }
     }
 }
